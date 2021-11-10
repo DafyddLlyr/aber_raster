0ae351a8c73926c4d36d4acc9989e62583ada656
DafyddLlyr <dafyddllyr@gmail.com>
2021-11-10 23:48:18 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,53 @@
+import './style.css';
+import Map from 'ol/Map';
+import OSM from 'ol/source/OSM';
+import TileLayer from 'ol/layer/Tile';
+import View from 'ol/View';
+import WMTS from 'ol/source/WMTS';
+import WMTSTileGrid from 'ol/tilegrid/WMTS';
+import {get as getProjection} from 'ol/proj';
+import {getTopLeft, getWidth} from 'ol/extent';
+import {optionsFromCapabilities} from 'ol/source/WMTS';
+
+const projection = getProjection('EPSG:3857');
+const projectionExtent = projection.getExtent();
+const size = getWidth(projectionExtent) / 256;
+const resolutions = new Array(19);
+const matrixIds = new Array(19);
+for (let z = 0; z < 19; ++z) {
+  // generate resolutions and matrixIds arrays for this WMTS
+  resolutions[z] = size / Math.pow(2, z);
+  matrixIds[z] = z;
+}
+
+const map = new Map({
+  layers: [
+    new TileLayer({
+      source: new OSM(),
+    }),
+    new TileLayer({
+      opacity: 0.7,
+      crossOrigin: 'anonymous',
+      source: new WMTS({
+        url: 'http://lle.gov.wales/services/tiles/lidar/wmts',
+        layer: '50cm_dsm',
+        matrixSet: 'GoogleMapsCompatible',
+        format: 'image/jpg',
+        crossOrigin: "anonomous",
+        projection: projection,
+        tileGrid: new WMTSTileGrid({
+          origin: getTopLeft(projectionExtent),
+          resolutions: resolutions,
+          matrixIds: matrixIds,
+        }),
+        style: 'default',
+        wrapX: true,
+      }),
+    }),
+  ],
+  target: 'map',
+  view: new View({
+    center: [-11158582, 4813697],
+    zoom: 4,
+  }),
+});
